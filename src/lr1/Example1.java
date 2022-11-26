62ba0024f34e78dba69698fdc5239f8664b8ca5c
featattach <featattach@gmail.com>
2022-11-26 10:59:53 +0500

    initial commit

--- src/lr1/Example1.java
+++ src/lr1/Example1.java
@@ -2,7 +2,10 @@ package lr1;
 
 public class Example1 {
     public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    System.out.println("Привет мир!");
+
 }
+
 
 }
---
+++ src/lr1/Example6.java
@@ -0,0 +1,11 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example6 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
