9494f7853898bac6c83a1ed6fa3b74d8a61f3776
featattach <featattach@gmail.com>
2022-12-11 01:03:09 +0500

    end lr3

--- src/lr3/ex.java
+++ src/lr3/ex.java
@@ -6,14 +6,12 @@ import java.util.Scanner;
 public class ex {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int x = 10, size = in.nextInt();
 
-        int count = 0;
-        int size = in.nextInt();
-
-        while (count < size){
-            System.out.println("count < 10, count = " + count);
-            count++;
+        for ( int i = 0; i < size; i++){
+            System.out.println("for | x < 10, x = " + i);
         }
+
 
     }
 }
