04cc0ba4c6ac8a68a472a3e4b09f51db58b5dab3
featattach <featattach@gmail.com>
2022-12-11 18:26:28 +0500

    end lr3 allmost

--- src/lr3/Example8.java
+++ src/lr3/Example8.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class Example8 {
     public static void main(String[] args) {
-        char[] letters = new char[10];
-        char a='A';
-        char sogl1 = 'A';
-        char sogl2 = 'E';
-        char sogl3 = 'I';
 
-        for (char i = 0; i < letters.length; ++i){
-
+        char mainmass[] = new char[10];
+        char i = 'A';
+        char massglas [] = {'A', 'E', 'I'};
+        for (int x = 0; x < mainmass.length; i++)
+        {
+            for (int y = 0; y < massglas.length; y++)
+            {
+                if (i == massglas[y]) {
+                    i++;
+                }
+            }
+            mainmass[x] = i;
+            System.out.println("Элемент массива ["+x+"] = " + mainmass[x]);
+            x++;
         }
-
     }
 }
+
 
 
 //    Напишите программу, в которой создается символьный массив из 10 элементов.
--- src/lr3/ex.java
+++ src/lr3/ex.java
@@ -6,16 +6,5 @@ import java.util.Scanner;
 
 public class ex {
     public static void main(String[] args) {
-       int x = 10, y = 3, z = 1, d;
-       int[] priroda = new int[14];
-       for (d = 0, d < priroda.length; d ++){
-           priroda[d] = d + x+y+z;
-       }
-       for (d = 0, d< priroda.length-1; d++){
-           priroda[d] = d - y;
-       }
-
     }
-
 }
-
