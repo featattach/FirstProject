1edc150b4e0e5c71611f01be4a9be3befbb66df3
featattach <featattach@gmail.com>
2022-12-21 11:43:37 +0500

    lr4

--- src/lr4/Example8.java
+++ src/lr4/Example8.java
@@ -1,22 +1,69 @@
 package lr4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Example8 {
+    private static class Ceaser {
+        static String encrTxt;
+
+        private static String encrypt(String enText, int key) {
+            char[] ArrayTxt = enText.toCharArray();
+            int[] ints = new int[ArrayTxt.length];
+            for (int i = 0; i < ArrayTxt.length; i++) {
+                ints[i] = ArrayTxt[i];
+                ints[i] = ints[i] + key;
+            }
+            for (int i = 0; i < ArrayTxt.length; i++) {
+                ArrayTxt[i] = (char) ints[i];
+            }
+            encrTxt = new String(ArrayTxt);
+            return encrTxt;
+        }
+
+
+        private static String decrypt(String enText, int key) {
+            char[] ArrayTxt = enText.toCharArray();
+            int[] ints = new int[ArrayTxt.length];
+            for (int i = 0; i < ArrayTxt.length; i++) {
+                ints[i] = ArrayTxt[i];
+                ints[i] = ints[i] - key;
+            }
+            for (int i = 0; i < ArrayTxt.length; i++) {
+                ArrayTxt[i] = (char) ints[i];
+            }
+            encrTxt = new String(ArrayTxt);
+            return encrTxt;
+        }
+    }
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст для шифрования");
+        String value = in.nextLine();
 
-
-
+        System.out.println("Введите ключ для шифрования");
+        int key = in.nextInt();
+        String encryptStr = Ceaser.encrypt(value, key);
+        System.out.print("Текст после проеобразования :" + encryptStr);
+        System.out.println("Выполнить обратное проеобразовани? (y/n)");
+        boolean iterator = false;
+        while (!iterator) {
+            String usanswer = in.next();
+            if (usanswer.toLowerCase().equals("y")) {
+                String decryptStr = Ceaser.decrypt(encryptStr, key);
+                System.out.println(decryptStr);
+                iterator = true;
+            } else if (usanswer.toLowerCase().equals("n")) {
+                System.out.println("До свидания!");
+                iterator = true;
+            } else {
+                System.out.println("Введите корректный ответ");
+                iterator = false;
+            }
+        }
     }
 }
 
-//
-//    Напишите программу «Шифр Цезаря», которая зашифровывает
-//        введенный текст. Используете кодовую таблицу символов. При запуске
-//        программы в консоль необходимо вывести сообщение: «Введите текст для
-//        шифрования», после ввода текста, появляется сообщение: «Введите ключ».
-//        После того как введены все данные, необходимо вывести преобразованную
-//        строку с сообщением «Текст после преобразования : ». Далее необходимо
-//        задать вопрос пользователю: «Выполнить обратное преобразование? (y/n)»,
-//        если пользователь вводит «y», тогда выполнить обратное преобразование.
-//        Если пользователь вводит «n», того программа выводит сообщение «До
-//        свидания!». Если пользователь вводит что-то другое, отличное от «y» или «n»,
-//        то программа ему выводит сообщение: «Введите корректный ответ».
+
+
