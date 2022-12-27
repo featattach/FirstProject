bf98e71da303751f3610d2fbd8980e97e685b104
featattach <featattach@gmail.com>
2022-12-27 21:51:12 +0500

    lr5 working

--- src/lr5/Example5.java
+++ src/lr5/Example5.java
@@ -12,4 +12,42 @@
 package lr5;
 
 public class Example5 {
+    public static void main(String[] args) {
+        Lab5 test05 = new Lab5(20);
+        System.out.println("Число "+ test05.Check());
+
+        test05.setA(15);
+        System.out.println("Число "+ test05.Check());
+        test05.setA(125);
+        System.out.println("Число "+ test05.Check());
+        test05.setA(-50);
+        System.out.println("Число "+ test05.Check());
+        test05.setA();
+        System.out.println("Число "+ test05.Check());
+    }
+
+}
+    class Lab5 {
+        private int a;
+
+        public void setA(int x) {
+            a = x;
+            if ( x >= 100){
+                a = 100;
+        }
+            if (x <= 0){
+                a = 0;
+            }
+    }
+    public void setA(){
+            a = 0;
+    }
+    public int Check(){
+            return a;
+        }
+        Lab5(int a){
+            this.setA(a);
+        }
+
+
 }
