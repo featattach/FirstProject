6703a614f6a88faa1fd6dce147e978fb9e73d644
featattach <featattach@gmail.com>
2023-01-15 00:12:36 +0500

    lr7 working

---
+++ src/timus/Task_2100/main.java
@@ -0,0 +1,17 @@
+package timus.Task_2100;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        String inputFileName = "src/timus/Task_2100/input.txt";
+
+        boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+        try{
+            System.out.println();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+}
